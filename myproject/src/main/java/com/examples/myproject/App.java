dd769ae359e59ffbdc1e6c7db829595b9c290018
Emgei <jacopomattei.fi@gmail.com>
2020-06-20 15:28:50 +0200

    Specificato build jacoco e coveralls solo per jdk8

--- myproject/src/main/java/com/examples/myproject/App.java
+++ myproject/src/main/java/com/examples/myproject/App.java
@@ -6,10 +6,6 @@ package com.examples.myproject;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World aa!" );
-    }
 
 	public String sayHello() {
 		return "Hello";
